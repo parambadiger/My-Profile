f19efe297831f1a773da12b25f6dc383dfe487ab
Paramesh Badiger <Parameshbadiger245@gmail.com>
2018-03-29 22:52:37 +0530

    added some images and modified index, main.css and main.js files

--- js/main.js
+++ js/main.js
@@ -1,12 +1,3 @@
-let navItems= ['#about', '#contact', '#academics', '#skill', '#experience'];
-function handleNavigation(e) {
-    var id =(e.target.getAttribute('data-value'))
-    navItems.map(function(item) {
-        if(id === item ) {
-            $(item).addClass("active");
-        }
-        else {
-        $(item).removeClass("active");
-        }
-    })
-}
+$(document).ready(function(){
+    document.getElementsByTagName('meta')['viewport'].content='min-width: 980px;';
+});
