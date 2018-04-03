cb86bb0900ba10d2a3b422c88aa2748ef3e4f35e
Paramesh Badiger <Parameshbadiger245@gmail.com>
2018-04-03 10:13:29 +0530

    modified index and stylesheet and js file

--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
-// $(document).ready(function(){
-//     document.getElementsByTagName('meta')['viewport'].content='min-width: 980px;';
-// });
+$(document).ready(function(){
+
+    $('aside #menu a').click(function(){
+        $('aside #menu a').removeClass('active');
+        $(this).addClass('active');
+        $('aside').toggleClass('active');
+        $('#open').toggleClass('active');
+        $('#close').toggleClass('active');
+    });
+
+    $('#open').click(function(){
+        $('aside').toggleClass('active');
+        $('#open').toggleClass('active');
+        $('#close').toggleClass('active');
+    });
+
+    $('#close').click(function(){
+        $('aside').toggleClass('active');
+        $('#open').toggleClass('active');
+        $('#close').toggleClass('active');
+    });
+
+    $('main').scroll(function() {
+        var scrollDistance = $('main').scrollTop();
+        $('.section').each(function(i) {
+            var top_width = $(this).position().top;
+            if ($(this).position().top <= scrollDistance) {
+                $('aside #menu a.active').removeClass('active');
+                $('aside #menu a').eq(i).addClass('active');
+            }
+        });
+    });
+});
